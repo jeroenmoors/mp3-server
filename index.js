19594e2f960b120296d10fdc11c1b79c72ef3df9
smhg <sam@strictlyphp.com>
2013-12-17 18:16:40 +0100

    pipe mp3 to client

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var BinaryServer = require('binaryjs').BinaryServer;
+var BinaryServer = require('binaryjs').BinaryServer,
+  fs = require('fs');
+
+var mp3 = fs.createReadStream('Jahzzar_-_03_-_Heartbreaker.mp3');
 
 var server = new BinaryServer({
   host: '127.0.0.1',
   port: 66600
+});
+
+server.on('connection', function (client) {
+  mp3.pipe(client.createStream());
 });
